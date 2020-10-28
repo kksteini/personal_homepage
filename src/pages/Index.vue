6172fe676620ef750f600be97a4e68164b73de7e
Skandalabrandur <steinerinn@gmail.com>
2020-10-28 14:46:11 +0000

    Changed around default files, changed colour schemes a little to reflect dark mode

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,10 +1,10 @@
 <template>
   <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-full.svg"
-    >
-  </q-page>
+    <div class="column">
+      <div class="col text-h3 text-center">Þorsteinn Sævar Kristjánsson</div>
+      <div class="col text-h5 text-center">Computer Scientist</div>
+    </div>
+ </q-page>
 </template>
 
 <script>
