73f39c5f62c31f02357a8fe4feef43c2e7c5e48d
Skandalabrandur <steinerinn@gmail.com>
2020-11-11 18:16:13 +0000

    Removing runtime away from the actions runner directory

---
+++ deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+if [ -d ~/ssr-server/dist ]; then
+    rm -rf ~/ssr-server/dist
+fi
+
+mkdir -p ~/ssr-server/
+cp -r ./dist ~/ssr-server/
+
+pm2 stop --name Homepage
+pm2 start ~/ssr-server/dist/index.js --name Homepage
